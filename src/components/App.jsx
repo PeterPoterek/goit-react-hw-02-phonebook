d6d9afd12bee39c06c3697d58e81b0a0c1e8f2b5
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-19 18:20:52 +0100

    number regex

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,30 @@ export const App = () => {
     contacts: [],
     name: '',
   });
-
   const handleAddContact = e => {
     e.preventDefault();
 
     const nameInput = e.target[0];
-
     const nameInputValue = nameInput.value;
     const namePattern = new RegExp(nameInput.pattern);
 
-    if (namePattern.test(nameInputValue)) {
+    const numberInput = e.target[1];
+    const numberInputValue = numberInput.value;
+    const numberPattern = new RegExp(numberInput.pattern);
+
+    if (
+      namePattern.test(nameInputValue) &&
+      numberPattern.test(numberInputValue)
+    ) {
       setState(prevState => ({
         ...prevState,
         contacts: [...prevState.contacts, nameInputValue],
       }));
     } else {
-      alert(nameInput.title);
+      const errorMessage = namePattern.test(nameInputValue)
+        ? numberInput.title
+        : nameInput.title;
+      alert(errorMessage);
     }
   };
 
