07a34ca9eb14d8d1ca26afeb204fba0fdff9f07a
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-20 18:49:23 +0100

    filter logic refactor

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,9 +2,14 @@ import ContactListItem from 'components/ContactListItem/ContactListItem.jsx';
 import { ContactListContainer, ContactListUl } from './ContactListStyles';
 
 const ContactList = ({ state, removeContact }) => {
-  const filteredContacts = state.contacts.filter(contact =>
-    contact.name.toLowerCase().includes(state.filter.toLowerCase())
-  );
+  const filteredContacts = state.contacts.filter(contact => {
+    const name = contact.name.toLowerCase();
+    const filterText = state.filter.toLowerCase();
+    return (
+      name.includes(filterText) ||
+      name.split(' ').some(part => part.startsWith(filterText))
+    );
+  });
 
   return (
     <ContactListContainer>
