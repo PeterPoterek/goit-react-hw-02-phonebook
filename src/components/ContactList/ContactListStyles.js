e6a293fe676da7a9bd5f33e5eb175206ab3a404a
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-20 18:25:37 +0100

    ContactListStyles

--- src/components/ContactList/ContactListStyles.js
+++ src/components/ContactList/ContactListStyles.js
@@ -1,4 +1,15 @@
 import styled from 'styled-components';
 
-export const ContactListContainer = styled.div``;
-export const ContactListUl = styled.ul``;
+export const ContactListContainer = styled.div`
+  max-width: 600px;
+  margin-top: 2rem;
+`;
+
+export const ContactListUl = styled.ul`
+  padding: 0;
+  margin: 0;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
